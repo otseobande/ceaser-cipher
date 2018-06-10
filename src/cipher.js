c9c1720e5f372f55a17ac381741f39d713c2325d
Otse Obande <otseobande@gmail.com>
2018-06-10 05:04:30 +0100

    Add encrypt and decrypt functions

---
+++ src/cipher.js
@@ -0,0 +1,40 @@
+const encrypt = (shift, text) => {
+  if(typeof shift !== 'number'){
+    throw TypeError('Shift should be a number');
+  }
+  if(typeof text !== 'string'){
+    throw TypeError('Text should be string');
+  }
+  
+  if(!text.match(/^[a-zA-z]+$/)){
+    throw Error('text must contain only alphabets');
+  }
+
+  const encryptedText = text.toUpperCase()
+    .split('')
+    .map((letter, index) => {
+      const charCode = text.charCodeAt(index);
+
+      const shiftSequence = charCode - shift - 65;
+      const modded = shiftSequence < 0 
+        ? 26 - Math.abs(shiftSequence) % 26 
+        : shiftSequence % 26;
+      const shiftedCharCode = modded + 65;
+
+      return String.fromCharCode(shiftedCharCode);
+    })
+    .join('');
+
+  return encryptedText
+};
+
+const decrypt = (shift, text) => {
+  const rightShift = shift * -1;
+
+  return encrypt(rightShift, text);
+};
+
+export {
+  encrypt,
+  decrypt
+}
--- tests/cipher.test.js
+++ tests/cipher.test.js
@@ -1,42 +1,29 @@
 import chai from 'chai';
-import { encrypt, decrypt } from '../js/cipher';
+import { encrypt, decrypt } from '../src/cipher';
 
 chai.should();
 
 describe('Ceaser cipher', () => {
   describe('encrypt method', () => {
-    it('should raise an error if arguments are less than two', () => {
-      (() => encrypt(3)).should.throw();
-    });
-
     it('should throw an error if first argument is not a number', () => {
-      (() => encrypt('3', 'andela')).should.throw(TypeError);
+      (() => encrypt('5', 'andela')).should.throw(TypeError);
     });
 
+    it('should throw an error if text contains special character', () => {
+      (() => encrypt(5, 'sdf,sdf')).should.throw();
+    })
     it('should throw an error if second argument is not a string', () => {
-      (() => encrypt(3, 43)).should.throw(TypeError);
+      (() => encrypt(5, 43)).should.throw(TypeError);
     });
 
     it('should encrypt a text properly', () => {
-      encrypt(3, 'ANDELA').should.be.equal('VIYZGV');
+      encrypt(5, 'ANDELA').should.be.equal('VIYZGV');
     });
   });
 
   describe('decrypt method', () => {
-    it('should raise an error if arguments are less than two', () => {
-      (() => decrypt(3)).should.throw();
-    });
-
-    it('should throw an error if first argument is not a number', () => {
-      (() => decrypt('3', 'andela')).should.throw(TypeError);
-    });
-
-    it('should throw an error if second argument is not a string', () => {
-      (() => decrypt(3, 43)).should.throw(TypeError);
-    });
-
-    it('should encrypt a text properly', () => {
-      decrypt(3, 'VIYZGV').should.be.equal('ANDELA');
+    it('should decrypt a text properly', () => {
+      decrypt(5, 'VIYZGV').should.be.equal('ANDELA');
     });
   })
 })
